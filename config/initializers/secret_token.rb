7c75f6bb3c728e179e5d4f9f1938c9f08f308b8e
chuvayva <chuvayva@gmail.com>
2013-06-17 23:30:17 +0400

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teamvillage::Application.config.secret_token = '5cf5ec171b750827a6bc00f93723914ee898091ba196ec55014255d083030c6b35ad4ef0940889ad673f5b44c8b2dd4fecd05d8959c5e0db74538874f110f85d'
