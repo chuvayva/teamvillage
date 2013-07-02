7480b06651f4205b790c246724b31e00d9df7d04
chuvayva <chuvayva@gmail.com>
2013-07-02 19:34:04 +0400

    assets fix

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,8 @@ module Teamvillage
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     config.generators do |g|
         g.text_framework :rspec, fixture: true
         g.fixture_replacement :factory_girl, dir: 'spec/factories'
