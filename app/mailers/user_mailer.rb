313832cbb32d4f1481d541ec8d3edb299a94e879
chuvayva <chuvayva@gmail.com>
2013-07-03 17:40:08 +0400

    mailer refac

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,19 +1,42 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
+  STYLESHEETS_PATH = 'public/assets/application.css' 
+
   def status(user)
+  	init_instance_vaiables_for user
+  	
+  	html = render_html_with instance_variables_hash
+
+		attachments['status.pdf'] = pdf_from html
+  	mail to: user.email, subject: 'Projects status'
+  end
+
+  private
+
+  def init_instance_vaiables_for(user)
   	@name = user.name
   	@owned_projects = Project.owned_by(user).includes(:tasks)
   	@paricipate_projects = Project.executed_by(user).includes(:tasks) 
   	@paricipate_projects -= @owned_projects
-  	
-  	html = render_to_string(template: 'user_mailer/status', layout: 'mail',locals: { :@name => @name, :@owned_projects => @owned_projects, :@paricipate_projects => @paricipate_projects })
+  end
 
+  def instance_variables_hash
+  	names = instance_variables.delete_if do |name| 
+  		name.to_s.start_with? '@_'
+  	end
+
+  	Hash[names.map{|v| [v, instance_eval(v.to_s)] }]
+  end
+
+  def render_html_with(locals)
+  	render_to_string(template: 'user_mailer/status', layout: 'mail',locals: locals)
+  end
+
+  def pdf_from(html)
 		kit = PDFKit.new(html)
-		kit.stylesheets << 'public/assets/application.css' 
+		kit.stylesheets << STYLESHEETS_PATH if File.exist? STYLESHEETS_PATH
 
-		attachments['status.pdf'] = kit.to_pdf
-
-  	mail to: user.email, subject: 'Projects status'
+		kit.to_pdf
   end
 end
